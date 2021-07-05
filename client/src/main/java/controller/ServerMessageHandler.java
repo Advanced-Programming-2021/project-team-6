687b84a1fdeab7b32e789c275a6767828bb34b7b
black-titan0 <alisafara@ce.sharif.edu>
2021-07-05 14:30:16 +0430

    profile pictures made better

--- client/src/main/java/controller/ServerMessageHandler.java
+++ client/src/main/java/controller/ServerMessageHandler.java
@@ -2,12 +2,13 @@ package controller;
 
 public class ServerMessageHandler {
 
-    public static String getServerMessage(String message) {
+   /* public static String getServerMessage(String message) {
         System.out.println("A Message Came From Central Server : " + message);
         switch (message) {
             case "GameOn" :
-                startGameScene();
+                //startGameScene();
                 break;
         }
-    }
+    }*/
 }
+
